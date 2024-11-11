26f1808c710c372c00f74dc7bb7c0b157e27e104
MisterIcing <alex.stephen02@gmail.com>
2024-11-10 23:52:46 -0500

    backend refactor

--- webgui/backend.py
+++ webgui/backend.py
@@ -8,13 +8,18 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 @app.route('/api/dryrun', methods=['POST'])
 def run_function():
     data = request.json
-    result = getDryRun(data)
+
+    # extra args (glob/exclude)
+    kwargs = {}
+    if data.get('glob'):
+        kwargs['glob_pattern'] = data['glob']
+    if data.get('exclude'):
+        kwargs['exclude_pattern'] = data['exclude']
+
+    fileNames = [f.name for f in get_files(data['url'], **kwargs)]
+    result = f"{"\n".join(fileNames)}"
+
     return jsonify(result=result)
-
-def getDryRun(search):
-    fileNames = [f.name for f in get_files(search['url'], glob_pattern=search['glob'], exclude_pattern=search['exclude'])]
-
-    return f"{"\n".join(fileNames)}"
 
 if __name__ == '__main__':
     app.run(port=5000)
